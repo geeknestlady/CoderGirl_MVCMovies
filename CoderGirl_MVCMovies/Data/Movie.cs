6414dd120835577488122d71e30f218c2b9a7138
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-05-01 17:27:59 -0500

    changes.

---
+++ CoderGirl_MVCMovies/Data/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoderGirl_MVCMovies
+{
+    public class Movie
+    {
+        public string MoviesName { get; set; }
+        public int ID { get; set; }
+        public decimal MovieRating { get; set; }
+    }
+}
---
+++ CoderGirl_MVCMovies/Data/MovieRatingRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoderGirl_MVCMovies.Data
+{
+    public class MovieRatingRepository : IMovieRatingRepository
+    {
+        public decimal GetAverageRatingByMovieName(string movieName)
+        {
+            decimal average = 
+        }
+
+        public List<int> GetIds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetMovieNameById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetRatingById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SaveRating(string movieName, int rating)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
