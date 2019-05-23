c5946e49e02c4223e8c6dbb26d9357be8945c68d
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-05-22 19:25:25 -0500

    In class changes, creating IModel Interface

---
+++ CoderGirl_MVCMovies/Data/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoderGirl_MVCMovies.Models;
+
+namespace CoderGirl_MVCMovies.Data
+{
+    public interface IRepository
+    {
+        IModel GetById(int id);
+        List<IModel> GetModels();
+        int Save(IModel model);
+        void Delete(int id);
+        void Update(IModel model);
+
+    }
+}
